bd56d43b09dfe110c28ff16f1b0148d551ae3268
Saidur Sikder <iam@saisikder.com>
2023-09-23 23:18:36 +0100

    Completed UI elements for Role Page

--- src/frontend/src/pages/RolePage.js
+++ src/frontend/src/pages/RolePage.js
@@ -1,31 +1,56 @@
 import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { RoleDetailCard } from '../components/RoleDetailCard';
 import { RoleSmallCard } from '../components/RoleSmallCard';
 
 export const RolePage = () => {
 
     const [role, setRole] = useState({});
+    const { roleName } = useParams();
 
     useEffect(
         () => {
             const fetchRoles = async () => {
-                const response = await fetch('http://localhost:8080/roles/Software Engineer');
-                const data = await response.json();
-                // console.log(data);
-                setRole(data);
-            }
+                try {
+                    const response = await fetch(`http://localhost:8080/roles/${roleName}`);
+                    if (!response.ok) {
+                        console.error(`Server responded with status: ${response.status}`);
+                        return;
+                    }
+                    
+                    const text = await response.text();
+                    
+                    if (!text) {
+                        console.warn("Received empty response from server");
+                        return;
+                    }
+                    
+                    const data = JSON.parse(text);
+                    setRole(data);
+                } catch (error) {
+                    console.error("An error occurred:", error);
+                }
+            };
             fetchRoles();
-        }, []
+        },
+        [roleName]
     );
 
-  return (
-    <div className="RolePage">
-      <h1>{role.roleName}</h1>
-      <h3>All skills</h3>
-      <RoleDetailCard />
-      {role.skills?.map((skill) => (
-                <RoleSmallCard key={skill.skillId} role={skill} />
-            ))}
-    </div>
-  );
+
+    if (!role || !role.roleName) {
+        return <h1>Role not found</h1>;
+    }
+    return (
+        <div className="RolePage">
+        <h1>{role.roleName}</h1>
+        <h3>All skills</h3>
+        {role.skills?.map((skill) => (
+                    <RoleDetailCard key={skill.skillId} skill={skill} />
+                ))}
+        <h3>All salaries</h3>
+        {role.salaries?.map((salary) => (
+                    <RoleSmallCard key={salary.salaryId} salary={salary} />
+                ))}
+        </div>
+    );
 }
